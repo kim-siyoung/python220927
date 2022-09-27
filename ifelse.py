32ebef3cdefd384c7a5daebb4cfa067102b8292f
sykim <kcysay12@gmail.com>
2022-09-27 11:29:47 +0900

    반복문에서 사용하는 함수들

--- ifelse.py
+++ ifelse.py
@@ -13,23 +13,53 @@
 
 # print("등급은 ",grade)
 
-value = 5
-while value > 0:
-    print(value)
-    value -= 1
+# value = 5
+# while value > 0:
+#     print(value)
+#     value -= 1
 
-d = {"apple":100, "orange":200}
-for item in d.items():
+# d = {"apple":100, "orange":200}
+# for item in d.items():
+#     print(item)
+
+# lst =[1,2,3,4,5,6,7,8,9,10]
+# for item in lst:
+#     if item > 5:
+#         break   # 탈출
+#     print("Item:{0}".format(item))
+
+# print("---------continue-----------")
+# for item in lst:
+#     if item % 2 == 0:
+#         continue  # 건너뛰기(스킵)
+#     print("Item:{0}".format(item))
+
+# print("---------range함수-----------")
+# print(list(range(10)))
+# print(list(range(1,11)))
+# print(list(range(2000,2023)))
+
+# print("---------리스트 내장-----------")
+# lst = list(range(1,11))
+# print ([i**2 for i in lst if i > 5])
+# tp = ("apple", "orange","kiwi")
+# print([len(i) for i in tp])
+
+print("---------필터링 없음-----------")
+lst = [20,25,30]
+iterL = filter(None, lst)
+for item in iterL:
     print(item)
 
-lst =[1,2,3,4,5,6,7,8,9,10]
-for item in lst:
-    if item > 5:
-        break   # 탈출
-    print("Item:{0}".format(item))
+print("---------필터링 있음-----------")
+def getBiggerThan20(i):
+    return i > 20
+lst = [20,25,30]
+iterL = filter(getBiggerThan20, lst)
+for item in iterL:
+    print(item)
 
-print("---------continue-----------")
-for item in lst:
-    if item % 2 == 0:
-        continue  # 건너뛰기(스킵)
-    print("Item:{0}".format(item))
+print("---------람다함수 사용-----------")
+iterL = filter(lambda x:x>20, lst) # 일회성으로 사용할 때 / 굳이 함수 선언? nono
+for item in iterL:
+    print(item)
