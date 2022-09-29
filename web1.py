6840d6a19d522c9843f116b6713a219dd093eb9a
sykim <kcysay12@gmail.com>
2022-09-29 10:40:16 +0900

    웹크롤링 연습

--- web1.py
+++ web1.py
@@ -6,4 +6,18 @@ from bs4 import BeautifulSoup
 page = open("c:\\work\\test01.html", "rt", encoding = "utf-8").read()
 # 검색이 용이한 객체
 soup = BeautifulSoup(page, "html.parser")
-print(soup.prettify())
+# print(soup.prettify())
+# 모든 <p> 검색
+# print(soup.find_all("p"))
+# 첫번째 <p> 검색
+# print(soup.find("p"))
+# <p class="outer-text">
+# print(soup.find_all("p", class_="outer-text"))
+# 특정id 검색
+# print(soup.find_all(id="first"))
+
+# 태그 내부에 컨텐츠만 검색
+for tag in soup.find_all("p"):
+    title = tag.text.strip()
+    title = title.replace("\n","")
+    print(title)
