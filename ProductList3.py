593b45e9599ed6c3ded9253c43b8e2e31678cf06
sykim <kcysay12@gmail.com>
2022-09-30 09:39:54 +0900

    제품리스트 연습

--- ProductList3.py
+++ ProductList3.py
@@ -20,7 +20,7 @@ form_class = uic.loadUiType("ProductList3.ui")[0]
 
 class Window(QMainWindow, form_class):
     def __init__(self):
-        super().__init__()
+        super().__init__() 
         self.setupUi(self)
         
         #초기값 셋팅 
